ed08bbee072665c08c46b9260efda367f0ee5281
Ibrahim Khaled <Ibrahimkhaled07@hotmail.com>
2026-04-28 00:54:48 +0300

    view basic implementation
    - view function in engine
    - link with the CLI thingy
    - fixing imports

--- interfaces/CMDI/view.py
+++ interfaces/CMDI/view.py
@@ -1,5 +1,6 @@
 import click
-
+from application import engine
+    
 @click.command()
 @click.option("--id", prompt = True, help = "price entry and exit based on ID")
 def view(id):
@@ -7,8 +8,4 @@ def view(id):
     this will send the request to the engine to get 
     the corresponding price entry and exit for the given ID and print the result
     """
-
-    click.echo(f"price entry exit result for ID: {id}")
-
-    #TODO: send the request to the engine
-    
+    engine.view_trade_record(id)
